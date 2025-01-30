13cd728fff925465c7cb57994916b4d9367e8997
Behnam Tehrani <behnam777@gmail.com>
2025-01-30 14:59:22 -0500

    fix route

--- bundle-api/src/app.ts
+++ bundle-api/src/app.ts
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define routes
-app.use('/service', ServiceRoutes);
+app.use('/bundle-api', ServiceRoutes);
 app.use('*', () => {
   throw new CustomError(404, 'Path not found.');
 });
