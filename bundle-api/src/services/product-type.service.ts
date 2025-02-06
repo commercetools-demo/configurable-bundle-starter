e6a89352cedfb879952326378b91c61c577b6a57
Behnam Tehrani <behnam777@gmail.com>
2025-02-06 16:26:43 -0500

    get all schemas and product-types

--- bundle-api/src/services/product-type.service.ts
+++ bundle-api/src/services/product-type.service.ts
@@ -4,7 +4,11 @@ import { createApiRoot } from '../client/create.client';
 export const getAllProductTypes = async (): Promise<ProductType[]> => {
   return await createApiRoot()
     .productTypes()
-    .get()
+    .get({
+      queryArgs: {
+        limit: 500
+      }
+    })
     .execute()
     .then((result) => {
       return result.body.count > 0 ? result.body.results : [];
--- bundle-api/src/services/schemas.service.ts
+++ bundle-api/src/services/schemas.service.ts
@@ -6,7 +6,11 @@ export const getAllSchemas = async (): Promise<SchemaCustomObject[]> => {
   return await createApiRoot()
     .customObjects()
     .withContainer({ container: SCHEMAS_CONTAINER })
-    .get()
+    .get({
+      queryArgs: {
+        limit: 500
+      }
+    })
     .execute()
     .then((result) => {
       return result.body.count > 0 ? result.body.results : [];
